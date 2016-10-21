8a0891cfee792ad63b6a19fe0b43411583088cf7
Mary Ruth Ngo <mngo@wellesley.edu>
2016-10-21 11:00:00 -0500

    changing persona to club HaQueen

--- edu/config.py
+++ edu/config.py
@@ -1,17 +1,12 @@
 import json
 
 all_data = {
-    "safwan": {
-        "persona_name": "Persona Name",
-        "label": "Subtitle",
-        "persona_description": """Lucinda is interested in lending her skills, and building new ones.
-                         She's here evaluating community, and looking for an opportunity to participate.
-                         She's might also be here looking for to connect with others around a cause she cares about.
-                         Perhaps, she was inspired to get involved after attending a Mozilla event or talk.
-                         Lucinda is engaged, but uncertain. <br><br>Lucinda cautiously lurks on a number of
-                         communication channels attempting to evaluate  friendliness, activity and opportunity.
-                         She follows what others post, but does not contribute to the conversation.
-                         If training and educational opportunities are offered, she will likely engage there first.""",
+    "GWC": {
+        "persona_name": "Girls Who Code",
+        "label": "high school club helps to pique girls' interests in tech early on",
+        "persona_description": """Girls Who Code is a nonprofit org that is built upon the notion of
+        closing the gender gap and for using tech for social good. Participants are young and inexperienced,
+        but take """,
 
         "persona_belonging" : "Outsider",
         "persona_image" : "http://tiptoes.ca/wp-content/uploads/2015/07/15065219318_9d22033cf4_k.jpg",

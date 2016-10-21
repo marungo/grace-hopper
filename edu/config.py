21ac72bb5bee36849f427a57eb620aa39325f19d
emmairwin <emma.irwin@gmail.com>
2016-10-21 10:49:36 -0500

    update variable

--- edu/config.py
+++ edu/config.py
@@ -1,35 +1,63 @@
 import json
 
 all_data = {
-    "safwan": {
-        "persona_name": "Persona Name",
-        "label": "Subtitle",
+    "lucinda": {
+        "persona_name": "Lucinda",
+        "label": "Project lurker",
         "persona_description": """Lucinda is interested in lending her skills, and building new ones.
-                         She's here evaluating community, and looking for an opportunity to participate.
-                         She's might also be here looking for to connect with others around a cause she cares about.
-                         Perhaps, she was inspired to get involved after attending a Mozilla event or talk.
-                         Lucinda is engaged, but uncertain. <br><br>Lucinda cautiously lurks on a number of
-                         communication channels attempting to evaluate  friendliness, activity and opportunity.
-                         She follows what others post, but does not contribute to the conversation.
-                         If training and educational opportunities are offered, she will likely engage there first.""",
+                         She's heard about Mozilla Clubs, and is taking a bit of time to research campus events.
+                         She's might  be here looking for to connect with others around a cause she cares about.
+                         Perhaps, she was inspired to get involved because she's interested in developing technical skills.
+                         Lucinda is intriqued, but uncertain. <br><br> What would help the most is an invitation to join in from someone she knows.""",
 
         "persona_belonging" : "Outsider",
         "persona_image" : "http://tiptoes.ca/wp-content/uploads/2015/07/15065219318_9d22033cf4_k.jpg",
-        "persona_qualities_layout": "lurkinglucinda",
-        "CCtitle": "Steampunk Fest Roma 2014",
-        "CClink": "https://www.flickr.com/photos/taymtaym/",
-        "CCname": "taymtaym",
+        "characteristic_1" : "fa fa-eye",
+        "characteristic_2" : "fa fa-heart",
+        "characteristic_3" : "fa-search",
+        "characteristic_4" : "fa-exclamation-triangle",
+        "inspiration_1" : "Changing the world",
+        "inspiration_2" : "Helping others",
+        "inspiration_3" : "Finding friends",
+        "project_barriers_1" : "Silent or dismissive communication",
+        "project_barriers_2" : "Skills/interest mismatch",
+        "project_barriers_3" : "No mentors",
+        "project_barriers_4" : "Frustration",
+        "project_opportunities_1" : "New perspective",
+        "project_opportunities_2" : "Fresh ideas",
+        "project_opportunities_3" : "Diversity",
+        "person_opportunities_1" : "Skill-building",
+        "person_opportunities_2" : "Mentorship",
+        "person_opportunities_3" : "Friendship",
+        "person_opportunities_4" : "Resume building",
+        "person_opportunities_5" : "Sense of purpose",
     },
-    "emma": {
-        "persona_name": "Emma Name",
-        "label": "My subtitle",
-        "persona_description": """hi there.""",
 
-        "persona_belonging" : "Outsider",
-        "persona_image" : "https://c1.staticflickr.com/3/2935/13505119194_173b2548f1_b.jpg",
-        "persona_qualities_layout": "lurkinglucinda",
-        "CCtitle": "Steampunk Fest Roma 2014",
-        "CClink": "https://www.flickr.com/photos/taymtaym/",
-        "CCname": "taymtaym",
+
+    "personaname": {
+        "persona_name": "",
+        "label": "Subtitle",
+        "persona_description": """""",
+        "persona_belonging" : "",
+        "persona_image" : "https://i.redditmedia.com/ElW6DNp4iefUaXLm_WIoEGhNfOmVPTQL3zYACd65GnM.jpg?w=1024&s=92cab568bff563c6ddb6ddf0a5dba9c8",
+        "characteristic_1" : "",
+        "characteristic_2" : "",
+        "characteristic_3" : "",
+        "characteristic_4" : "",
+        "inspiration_1" : "",
+        "inspiration_2" : "",
+        "inspiration_3" : "",
+        "project_barriers_1" : "",
+        "project_barriers_2" : "",
+        "project_barriers_3" : "",
+        "project_barriers_4" : "",
+        "project_opportunities_1" : "",
+        "project_opportunities_2" : "",
+        "project_opportunities_3" : "",
+        "person_opportunities_1" : "",
+        "person_opportunities_2" : "",
+        "person_opportunities_3" : "",
+        "person_opportunities_4" : "",
+        "person_opportunities_5" : "",
     },
 }
